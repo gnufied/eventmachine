9dadc2c8f32365e5ddccdf189820d2e1f013454e
gnufied <gethemant@gmail.com>
2009-05-04 02:27:50 +0530

    there is no need to derive from java class in ruby Connection class

--- lib/jeventmachine.rb
+++ lib/jeventmachine.rb
@@ -48,11 +48,11 @@ module EventMachine
       EventMachine::event_callback a1, a2, s
     end
   end
-  class Connection < com.rubyeventmachine.Connection
-    def associate_callback_target sig
-      # No-op for the time being.
-    end
-  end
+  # class Connection < com.rubyeventmachine.Connection
+  #   def associate_callback_target sig
+  #     # No-op for the time being.
+  #   end
+  # end
   def self.initialize_event_machine
     @em = JEM.new
   end
